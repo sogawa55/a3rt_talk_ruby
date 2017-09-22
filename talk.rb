3e09da352d3121580fe05cb256dcd69a32aa0a83
sogawa <s.ogawa055@gmail.com>
2017-09-22 20:13:33 +0000

    modified

--- talk.rb
+++ talk.rb
@@ -8,13 +8,15 @@ require 'faraday_middleware'
 URL_Recruit = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
 KEY_Recruit = 'mkxumAgVxDuMXYF6EyHPKyPUrLGD2Cun'
 
+message = ARGV[0]
+
 conn = Faraday::Connection.new(:url => URL_Recruit ) do |builder|
     builder.use Faraday::Request::UrlEncoded
     builder.use Faraday::Response::Logger
     builder.use Faraday::Adapter::NetHttp
 end
 
-params = {apikey: KEY_Recruit, query: "おはよう"}
+params = {apikey: KEY_Recruit, query: message}
 
 response = conn.post URL_Recruit, params
 json = JSON.parse(response.body)
